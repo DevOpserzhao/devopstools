e4095f6d5792dea8d930d9cec47a688fba007e1e
devops_init <zxfleon@163.com>
2023-05-31 18:19:12 +0800

    add java  app demo

---
+++ devopsAppDemo/java/java-maven-sample/src/main/java/com/devops/javamavensample/JavaMavenSampleApplication.java
@@ -0,0 +1,13 @@
+package com.devops.javamavensample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMavenSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMavenSampleApplication.class, args);
+    }
+
+}
---
+++ devopsAppDemo/java/java-maven-sample/src/test/java/com/devops/javamavensample/JavaMavenSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.javamavensample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMavenSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
